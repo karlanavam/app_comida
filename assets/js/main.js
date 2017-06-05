750f602796f028d26bb2a96fd444d83065c853a6
KarlaNavaM <karla.nm8@gmail.com>
2017-06-05 15:43:07 -0500

    Terminé v0.0.1: mapa y current position

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,33 @@
-$(document).ready(cargarPagina);
+/*19.4176387,-99.167004*/
 
+var obtenerUbicacion = function (e) {
+	if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(mostrarPosicion);
+	} else {
+		alert("Actualice su navegador");
+	}
+};
 
-var cargarPagina = function() {
-    $("get-location").click(obtenerUbicacion);
+var mostrarPosicion = function (posicion) {
+	console.log(posicion);
 
-function obtenerUbicacion() {
-    if (navigator.geolocation.getCurrentPosition(
-    mostrarPosicion))
+	var coordenadas = {
+		lat: posicion.coords.latitude, 
+		lng: posicion.coords.longitude
+	};
     
+	mostrarMapa(coordenadas);
+};
+
+var mostrarMapa = function (coordenadas) {
+	var map = new google.maps.Map($('#map')[0], {
+      zoom: 16,
+      center: coordenadas
+    });
+    var marker = new google.maps.Marker({
+      position: coordenadas,
+      map: map
+    });
 }
+
+$(document).ready(obtenerUbicacion);
