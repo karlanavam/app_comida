983ebad2e300e39bee2d74e87e429ed4a3c18445
KarlaNavaM <karla.nm8@gmail.com>
2017-06-06 10:53:50 -0500

    Agregué data lat y lng, para agregar funcionalidad en ver mapa

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,8 +37,8 @@ var restaurantes = [
         "direccion": "Av de los Insurgentes Sur 895, Nápoles, 03840 Ciudad de México, CDMX",
         "categoria": "mexicana",
         "coordenadas": {
-            lng: "19.4142741", 
-		    lat: "-99.1884862"
+            lng: -99.1884862, 
+		    lat: 19.4142741
         }
     },
     {
@@ -46,8 +46,8 @@ var restaurantes = [
         "direccion": "Avenida Patriotismo 229, San Pedro de los Pinos, 03800 Benito Juarez, CDMX",
         "categoria": "mexicana",
         "coordenadas": {
-            lng: "19.3970436", 
-		    lat: "-99.182696"
+            lng: -99.182696, 
+		    lat: 19.3970436
         }
     },
     {
@@ -55,8 +55,8 @@ var restaurantes = [
         "direccion": "Av de los Insurgentes Sur 746, Col del Valle Centro, 03100 Ciudad de México, CDMX",
         "categoria": "oriental",
         "coordenadas": {
-            lng: "19.39181", 
-		    lat: "-99.1750627"
+            lng: -99.1750627, 
+		    lat: 19.39181
         }
     },
     {
@@ -64,8 +64,8 @@ var restaurantes = [
         "direccion": "Félix Cuevas 374 Local 107, Benito Juárez, Del Valle, Tlacoquemecatl del Valle, 03200 Ciudad de México, CDMX",
         "categoria": "oriental",
         "coordenadas": {
-            lng: "19.3728328", 
-		    lat: "-99.1750115"
+            lng: -99.1750115, 
+		    lat: 19.3728328
         }
     },
     {
@@ -73,8 +73,8 @@ var restaurantes = [
         "direccion": "Av Insurgentes Sur 729, Benito Juarez, Nápoles, 03810 Ciudad de México, CDMX",
         "categoria": "Italiana",
         "coordenadas": {
-            lng: "19.3923816", 
-		    lat: "-99.17541"
+            lng: -99.17541, 
+		    lat: 19.3923816
         }
     },
     {
@@ -82,8 +82,8 @@ var restaurantes = [
         "direccion": "Av. Popocatepetl 546 Local J-11, Benito Juárez, Xoco, 03340 Ciudad de México, CDMX",
         "categoria": "italiana",
         "coordenadas": {
-            lng: "19.3889858", 
-		    lat: "-99.2083713"
+            lng: -99.2083713, 
+		    lat: 19.3889858
         }
     }
 ];
@@ -101,7 +101,7 @@ var plantilla = '<article class="item-restaurante">' +
                       '<p>Comida __categoria__</p>' +
                     '</div>' +
                 '<div class="card-action">' +
-                  '<a href="#">Ver mapa</a>' +
+                  '<a href="#" data-lat="__lat__" data-lng="__lng__" class="cambiarMapa">Ver mapa</a>' +
                '</div>' +
                 '</div>' +
             '</div>' +
@@ -110,17 +110,59 @@ var plantilla = '<article class="item-restaurante">' +
 
 var mostrarRestaurantes = function (restaurantes) {
 	var plantillaFinal = "";
-	restaurantes.forEach(function (popo) {
-		plantillaFinal += plantilla.replace("__nombre__", popo.nombre)
-			.replace("__direccion__", popo.direccion)
-			.replace("__categoria__", popo.categoria);
+	restaurantes.forEach(function (comer) {
+		plantillaFinal += plantilla.replace("__nombre__", comer.nombre)
+                             .replace("__direccion__", comer.direccion)
+                             .replace("__categoria__", comer.categoria)
+                            .replace("__lat__", comer.coordenadas.lat)
+                            .replace("__lng__", comer.coordenadas.lng);
 	});
 	$("#lista-restaurantes").html(plantillaFinal);
+}
+
+var filtrarRestaurantes = function (e) {
+	e.preventDefault();
+	var criterioBusqueda = $("#search").val().toLowerCase();
+	var contactosFiltrados = restaurantes.filter(function (restaurantes) {
+		return restaurantes.nombre.toLowerCase().indexOf(criterioBusqueda) >= 0;
+	});
+	mostrarContactos(contactosFiltrados);
+};
+
+function cambiarUbicacion() {
+  var latitud = $(this).data("lat");
+  var longitud = $(this).data("lng");
+
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
+  };
+
+  console.log(coordenadas);
+  mostrarMapa(coordenadas);
 }
 
 function cargarPagina() {
     obtenerUbicacion();
     mostrarRestaurantes(restaurantes);
+    $('.cambiarMapa').click(cambiarUbicacion);
 }
 
 $(document).ready(cargarPagina);
+
+
+/*
+darle una clase al elemento .restaurante y también ponerle el atributo data-lat data-long
+
+agregar data-log y data-lat en la plantilla, poner una clase para remplazar en el each.
+
+en funcion cargarPagina:
+meter la funion: cambiarUbicación();
+
+function cambiar ubicación() {
+    
+}
+
+
+*/
+
